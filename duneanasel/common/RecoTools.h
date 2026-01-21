514b6a38df84ac8132084bc17b4fb88c8ff3da0d
Luke Pickering <luketpickering@gmail.com>
2026-01-21 10:12:21 +0000

    recotools tweaks

--- duneanasel/common/RecoTools.h
+++ duneanasel/common/RecoTools.h
@@ -16,16 +16,12 @@ inline double ParticleLength(T const &p) {
                     p.end.z - p.start.z);
 }
 
-// the auto return type here lets the compiler figure out whether to return a
-//  caf::SRRecoParticle * or a caf::SRRecoParticleProxy * for a given
-//  instantiation
 template <typename T, typename C = Proxyable_t<caf::SRInteraction, T>>
-inline auto const *GetLongestParticle(T const &nd_int,
-                                      RecoBranch reco = kSPINE) {
-
-  // this looks hideous, but can be thought of as a generalisation of the
-  // ternary operator.
-  decltype(nd_int.part.pandora) const *parts = nullptr;
+inline auto const *GetRecoParticles(T const &nd_int, RecoBranch reco = kSPINE) {
+  auto const *parts =
+      &nd_int.part.pandora; // set it to pandora first so that we can get the
+                            // right template-dependent type, then set it to the
+                            // one we really want.
   switch (reco) {
   case kPandora: {
     parts = &nd_int.part.pandora;
@@ -39,6 +35,18 @@ inline auto const *GetLongestParticle(T const &nd_int,
     throw std::runtime_error("Invalid RecoBranch specified.");
   }
   }
+
+  return parts;
+}
+
+// the auto return type here lets the compiler figure out whether to return a
+//  caf::SRRecoParticle * or a caf::SRRecoParticleProxy * for a given
+//  instantiation
+template <typename T, typename C = Proxyable_t<caf::SRInteraction, T>>
+inline auto const *GetLongestParticle(T const &nd_int,
+                                      RecoBranch reco = kSPINE) {
+
+  auto parts = GetRecoParticles(nd_int, reco);
 
   double longest = -std::numeric_limits<double>::max();
   size_t longest_idx = std::numeric_limits<size_t>::max();
