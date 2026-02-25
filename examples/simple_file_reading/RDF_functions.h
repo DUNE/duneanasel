d9ad4d03b2121dbe8cccab3831ea41cf95c35cee
fbattist <federico.battisti@bo.infn.it>
2026-02-25 16:37:01 +0100

    RDF example is now in a good state

--- examples/simple_file_reading/RDF_functions.h
+++ examples/simple_file_reading/RDF_functions.h
@@ -3,64 +3,38 @@
 #include "duneanaobj/StandardRecord/SRTrueParticle.h"
 #include "duneanaobj/StandardRecord/SRVector3D.h"
 #include "duneanaobj/StandardRecord/SRLorentzVector.h"
-ROOT::VecOps::RVec<Float_t> nu_prop_by_pdg(const ROOT::VecOps::RVec<Float_t>& p, const ROOT::VecOps::RVec<Int_t>& pdg, Int_t pdg_target) {
+ROOT::VecOps::RVec<Float_t> nu_E_by_pdg(const ROOT::VecOps::RVec<caf::SRTrueInteraction>& p, Int_t pdg_target) {
 
     ROOT::VecOps::RVec<Float_t> OUT;
-
-    for (size_t i = 0; i < p.size(); ++i) {
-        if (pdg[i] == pdg_target) {
-            OUT.push_back(p[i]);
+    for (const auto & nu : p) {
+        if (nu.pdg == pdg_target) {
+            OUT.push_back(nu.E);
         }
     }
 
     return OUT;
 }
 
-ROOT::VecOps::RVec<Int_t> nu_prop_by_pdg(const ROOT::VecOps::RVec<Int_t>& p, const ROOT::VecOps::RVec<Int_t>& pdg, Int_t pdg_target) {
-
-    ROOT::VecOps::RVec<Int_t> OUT;
-
-    for (size_t i = 0; i < p.size(); ++i) {
-        if (pdg[i] == pdg_target) {
-            OUT.push_back(p[i]);
-        }
-    }
-
-    return OUT;
-}
-
-ROOT::VecOps::RVec<caf::SRLorentzVector> nu_prop_by_pdg(const ROOT::VecOps::RVec<caf::SRLorentzVector>& p, const ROOT::VecOps::RVec<Int_t>& pdg, Int_t pdg_target) {
-
-    ROOT::VecOps::RVec<caf::SRLorentzVector> OUT;
-
-    for (size_t i = 0; i < p.size(); ++i) {
-        if (pdg[i] == pdg_target) {
-            OUT.push_back(p[i]);
-        }
-    }
-
-    return OUT;
-}
-
-ROOT::VecOps::RVec<caf::SRVector3D> nu_prop_by_pdg(const ROOT::VecOps::RVec<caf::SRVector3D>& p, const ROOT::VecOps::RVec<Int_t>& pdg, Int_t pdg_target) {
-
-    ROOT::VecOps::RVec<caf::SRVector3D> OUT;
-
-    for (size_t i = 0; i < p.size(); ++i) {
-        if (pdg[i] == pdg_target) {
-            OUT.push_back(p[i]);
-        }
-    }
-
-    return OUT;
-}
-
-ROOT::VecOps::RVec<Int_t> prim_pdg(const ROOT::VecOps::RVec<caf::SRTrueInteraction>& p) {
+ROOT::VecOps::RVec<Int_t> prim_pdg(const ROOT::VecOps::RVec<caf::SRTrueInteraction>& p, Int_t nu_pdg) {
     ROOT::VecOps::RVec<Int_t> OUT ;
 
-    for (const auto & prim_vec : p) {
-            for (const auto & prim : prim_vec.prim) {
+    for (const auto & inter : p) {
+        if (inter.pdg != nu_pdg) continue;
+        for (const auto & prim : inter.prim) {
                 OUT.push_back(prim.pdg);
+        } 
+    }
+
+    return OUT;
+}
+
+ROOT::VecOps::RVec<Float_t> prim_p_by_pdg(const ROOT::VecOps::RVec<caf::SRTrueInteraction>& p, Int_t nu_pdg, Int_t prim_pdg) {
+    ROOT::VecOps::RVec<Float_t> OUT ;
+
+    for (const auto & inter : p) {
+        if (inter.pdg != nu_pdg) continue;
+        for (const auto & prim : inter.prim) {
+            if(prim.pdg == prim_pdg) OUT.push_back(prim.p.Mag());
         } 
     }
 
