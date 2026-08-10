2b63002005bbfc154b1e4992e8ee0d9f7199653c
Luke Pickering <luketpickering@gmail.com>
2026-08-10 16:32:30 +0100

    need one more level of indirection in truth-searching

--- duneanasel/common/TruthTools.h
+++ duneanasel/common/TruthTools.h
@@ -8,8 +8,12 @@ inline size_t GetTrueInteractionIndex(caf::SRInteraction const &nu_int) {
   if (!nu_int.truthOverlap.size()) {
     return kCannotFindTruth;
   }
-  return std::distance(
+  size_t idx = std::distance(
       nu_int.truthOverlap.begin(),
       std::max_element(nu_int.truthOverlap.begin(), nu_int.truthOverlap.end()));
+  if (idx >= nu_int.truth.size()) {
+    return kCannotFindTruth;
+  }
+  return nu_int.truth[idx];
 }
 } // namespace ana
