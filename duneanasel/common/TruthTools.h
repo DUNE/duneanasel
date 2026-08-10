4a325ad415b6716f5817e6f55ec49426ddcd038e
Luke Pickering <luketpickering@gmail.com>
2026-08-10 15:20:16 +0100

    adds GetTrueInteractionIndex helper to common/TruthTools

--- duneanasel/common/TruthTools.h
+++ duneanasel/common/TruthTools.h
@@ -1,10 +1,15 @@
-#include "SRInteraction.h"
+#include "duneanaobj/StandardRecord/SRInteraction.h"
 
 namespace ana {
 
-inline auto GetTrueInteraction(caf::SRInteraction const &nu_int) {
-  auto max = std::max_element(nu_int.)
+constexpr static auto kCannotFindTruth = std::numeric_limits<size_t>::max();
+
+inline size_t GetTrueInteractionIndex(caf::SRInteraction const &nu_int) {
+  if (!nu_int.truthOverlap.size()) {
+    return kCannotFindTruth;
+  }
+  return std::distance(
+      nu_int.truthOverlap.begin(),
+      std::max_element(nu_int.truthOverlap.begin(), nu_int.truthOverlap.end()));
 }
-
 } // namespace ana
-
